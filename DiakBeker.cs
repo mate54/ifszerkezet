8b4582fbfb6f6f23453c70e9ca7c7242b701f041
mate54 <mt919hun@gmail.com>
2021-09-20 13:13:24 +0200

    első

---
+++ DiakBeker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifszerkezet
+{
+    class DiakBeker
+    {
+        public string Nev { get; set; }
+        public int EletKor { get; set; }
+
+        public DiakBeker(string nev, int eletkor)
+        {
+            this.Nev = nev;
+            this.EletKor = eletkor;
+        }
+
+        public void AdatKiir()
+        {
+            Console.WriteLine($"Név: {this.Nev} és életkor: {this.EletKor}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifszerkezet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DiakBeker diak = new DiakBeker("Márkus Regő", 14);
+
+            Console.WriteLine(diak.Nev);
+            Console.WriteLine(diak.EletKor);
+
+            diak.AdatKiir();
+
+            Console.ReadKey(true);
+        }
+    }
+}
